6f3252e3a94b86c99df056015f1b04ab2654ca7b
T_cat9000_2 <terpstragraham@gmail.com>
2024-09-06 10:30:51 -0700

    Update command-line-open.py

--- src/command-line-open.py
+++ src/command-line-open.py
@@ -1,6 +1,9 @@
 import webbrowser
-print("welcome this is a application that helps you open github repo's straight from the command line.")
-print("just enter the url and we will bring you there!")
-url = input("URL:")
-"free" in url
-webbrowser.open(url)
+while True:
+  print("welcome this is a application that helps you open github repo's straight from the command line.")
+  print("just enter the url and we will bring you there!")
+  url = input("URL:")
+  if "github.com" in url:
+    webbrowser.open(url)
+  else:
+    print("url has to be to github.com! try something like github.com/alter-net-codes")
