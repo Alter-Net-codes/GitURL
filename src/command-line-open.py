85e1744fb925a35a210d70b08b74a2281c7c1d3f
T_cat9000_2 <terpstragraham@gmail.com>
2024-09-06 10:39:36 -0700

    Update command-line-open.py

--- src/command-line-open.py
+++ src/command-line-open.py
@@ -1,9 +1,9 @@
 import webbrowser
 
-while True:
-    print("Welcome! This application helps you open GitHub repos straight from the command line.")
-    print("Just enter the URL, and we will bring you there!")
-    
+print("Welcome! This application helps you open GitHub repos straight from the command line.")
+print("Just enter the URL, and we will bring you there!")
+
+while True:  
     url = input("URL: ").strip()
 
     # Check if the input contains exactly 'github.com'
