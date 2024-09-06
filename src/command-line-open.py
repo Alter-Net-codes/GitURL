c0954e1ed4b500fea0829dcc6e95bce88883a27c
T_cat9000_2 <terpstragraham@gmail.com>
2024-09-06 10:28:40 -0700

    Update command-line-open.py

--- src/command-line-open.py
+++ src/command-line-open.py
@@ -2,4 +2,5 @@ import webbrowser
 print("welcome this is a application that helps you open github repo's straight from the command line.")
 print("just enter the url and we will bring you there!")
 url = input("URL:")
+"free" in url
 webbrowser.open(url)
