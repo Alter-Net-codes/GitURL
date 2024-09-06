c8f5223e69480ee463a3a7e6637aa3f562b0f90c
T_cat9000_2 <terpstragraham@gmail.com>
2024-09-06 10:38:04 -0700

    Update command-line-open.py

--- src/command-line-open.py
+++ src/command-line-open.py
@@ -1,9 +1,13 @@
 import webbrowser
+
 while True:
-  print("welcome this is a application that helps you open github repo's straight from the command line.")
-  print("just enter the url and we will bring you there!")
-  url = input("URL:")
-  if "github.com" in url:
-    webbrowser.open(url)
-  else:
-    print("url has to be to github.com! try something like github.com/alter-net-codes")
+    print("Welcome! This application helps you open GitHub repos straight from the command line.")
+    print("Just enter the URL, and we will bring you there!")
+    
+    url = input("URL: ").strip()
+
+    # Check if the input contains exactly 'github.com'
+    if url.startswith("https://github.com") or url.startswith("http://github.com"):
+        webbrowser.open(url)
+    else:
+        print("The URL has to be a GitHub link! Try something like https://github.com/alter-net-codes")
